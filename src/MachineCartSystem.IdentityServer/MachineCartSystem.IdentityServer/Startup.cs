4f6b80b475095a411e01011f37c9ac964aec050d
Bheemraj <gehlot189@gmail.com>
2020-11-08 12:37:06 +0530

    commit

---
+++ src/MachineCartSystem.IdentityServer/MachineCartSystem.IdentityServer/Startup.cs
@@ -0,0 +1,91 @@
+using IdentityServerHost.Quickstart.UI;
+using MachineCartSystem.IdentityServer.Service;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace MachineCartSystem.IdentityServer
+{
+    public class Startup
+    {
+        public IWebHostEnvironment Environment { get; }
+        public IConfiguration Configuration { get; }
+
+        public Startup(IWebHostEnvironment environment, IConfiguration configuration)
+        {
+            Environment = environment;
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(p => p.AddPolicy("AllowOrigin", options =>
+            {
+                options.AllowAnyOrigin();
+                options.AllowAnyHeader();
+                options.AllowAnyMethod();
+            }));
+
+            services.AddControllersWithViews();
+            
+            services.AddIdentityServer(p=>
+            {
+                p.Discovery.ResponseCacheInterval = 0;
+               // p.Endpoints.
+            })
+                .AddInMemoryIdentityResources(Config.Ids)
+                .AddInMemoryApiResources(Config.Apis)
+                .AddInMemoryApiScopes(Config.ApiScopes)
+                .AddInMemoryClients(Config.Clients)
+                .AddDeveloperSigningCredential()
+                .AddTestUsers(TestUsers.Users);
+
+            services.AddAuthentication("MyCookie")
+                .AddCookie("MyCookie", options =>
+                {
+                    options.Cookie.HttpOnly = true;
+                    options.ExpireTimeSpan = TimeSpan.FromSeconds(14000);
+                    //options.LoginPath = "/Identity/Account/Login";
+                    //options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+                    options.SlidingExpiration = true;
+                });
+
+            // services.AddScoped<IProfileService, ProfileService>();
+
+            //services.ConfigureApplicationCookie(options =>
+            //{
+            //    // Cookie settings
+            //    options.Cookie.HttpOnly = true;
+            //    options.ExpireTimeSpan = TimeSpan.FromSeconds(10);
+            //    //options.LoginPath = "/Identity/Account/Login";
+            //    //options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+            //    options.SlidingExpiration = true;
+            //});
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseCors("AllowOrigin");
+            app.UseStaticFiles();
+            //    app.UseSerilogRequestLogging();
+            app.UseRouting();
+            app.UseIdentityServer();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
---
+++ src/MachineCartSystem.UI/projects/ui/src/app/pages/error/unauthorize/unauthorize.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit,OnDestroy } from '@angular/core';
+
+@Component({
+  selector: 'app-unauthorize',
+  templateUrl: './unauthorize.component.html',
+  styleUrls: ['./unauthorize.component.css']
+})
+export class UnauthorizeComponent implements OnInit,OnDestroy {
+
+  constructor() { }
+
+  ngOnDestroy(): void {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+}

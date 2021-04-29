29847e42fb58f6e187cede6e88e2214c8f85b5df
Bheemraj <gehlot189@gmail.com>
2021-04-29 22:57:04 +0530

    commit

--- src/MachineCartSystem.Core/MachineCartSystem.Configuration/BaseStartup.cs
+++ src/MachineCartSystem.Core/MachineCartSystem.Configuration/BaseStartup.cs
@@ -1,11 +1,13 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Linq;
 
 namespace MachineCartSystem.Configuration
 {
     public abstract class BaseStartup
     {
         protected readonly IConfiguration Configuration;
+        protected ApiName _apiName;
 
         public BaseStartup(IConfiguration configuration)
         {
@@ -14,7 +16,6 @@ namespace MachineCartSystem.Configuration
 
         public DbConfig DbConfig => GetDbConfigSetting();
         public JwtConfig JwtConfig => JwtConfigSetting();
-
         private DbConfig GetDbConfigSetting()
         {
             var attemptSection = Configuration.GetSection("DbConfiguration:Attempt");
@@ -28,6 +29,30 @@ namespace MachineCartSystem.Configuration
 
         private JwtConfig JwtConfigSetting()
         {
+            if (_apiName == ApiName.Gateway)
+            {
+                var lstApi = Enum.GetNames(typeof(ApiName)).Where(p => p != ApiName.Gateway.ToString()).ToList();
+
+                var scopes = new string[] { Configuration[ApiName.Gateway.ToString() + ":Scopes"] };
+                var audiences = new string[] { Configuration[ApiName.Gateway.ToString() + ":Audiences"] };
+
+                lstApi.ForEach(p =>
+                {
+                    scopes.Append(" " + Configuration[p + ":Scopes"]);
+                    audiences.Append(" " + Configuration[p + ":Audiences"]);
+                });
+
+                var jwtConfig = new JwtConfig
+                {
+                    Audiences = audiences,
+                    Authority = Configuration["Identity:Authority"],
+                    Issuer = Configuration["Identity:Issuer"],
+                    //Key = Configuration["PrivateKey"]?.ToCharArray(),
+                    Scopes = scopes
+                };
+                return jwtConfig;
+            }
+
             return new JwtConfig
             {
                 Audiences = Configuration["Audiences"]?.Split(new char[] { ',' }),
